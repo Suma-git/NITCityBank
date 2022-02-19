a63fca3d65398694abc38bcfc25c3be218ffa230
uppara <sumauppara.123@gmail.com>
2022-02-19 15:28:12 -0500

    ClassA1 added

---
+++ DemoProj1/src/com/nt/test/ClassA1.java
@@ -0,0 +1,32 @@
+package com.nt.test;
+
+public class ClassA1 {
+
+	static int a=0;
+	int b=0;
+
+	ClassA1()
+	{
+	a++;
+	b++;
+	System.out.println("static var-->"+a);
+	System.out.println("instance var--->"+b);
+	System.out.println("___________");
+	}
+	void display()
+	{
+	System.out.println("Accessing static var");
+	System.out.println(ClassA1.a);
+	System.out.println(a);
+	System.out.println(new ClassA1().a);
+	}
+
+	public static void main(String[] args){
+	
+	new ClassA1();
+	new ClassA1();
+	new ClassA1();
+	System.out.println("##############");
+	new ClassA1().display();
+	}
+}
